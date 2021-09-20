6e8215d24b70803b82afcbb539f76aafed0aa74c
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-20 13:07:31 +0300

    Update common module
    - Add @Service annotation to UserServiceImpl

--- common/src/main/java/com/wcdevs/blog/core/common/user/UserServiceImpl.java
+++ common/src/main/java/com/wcdevs/blog/core/common/user/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.wcdevs.blog.core.common.user;
 
 import com.wcdevs.blog.core.persistence.user.UserDto;
 import com.wcdevs.blog.core.persistence.user.UserRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * Default {@link UserService} implementation.
  */
+@Service
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
 
