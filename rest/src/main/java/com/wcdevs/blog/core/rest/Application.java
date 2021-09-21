af9591f80eee870b1cc243acde4de28e93a7fa9f
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-21 11:24:05 +0300

    Update rest module
    - Add packages to be scanned by SB app

--- rest/src/main/java/com/wcdevs/blog/core/rest/Application.java
+++ rest/src/main/java/com/wcdevs/blog/core/rest/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Application entry point.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.wcdevs.blog.core")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
