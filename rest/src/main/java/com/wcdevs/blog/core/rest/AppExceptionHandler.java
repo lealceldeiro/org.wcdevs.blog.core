dc758ce7018ff03ebccefea02f11f20d4a7f0415
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-21 15:00:10 +0300

    Update rest module
    - Fix wrong import order

--- rest/src/main/java/com/wcdevs/blog/core/rest/AppExceptionHandler.java
+++ rest/src/main/java/com/wcdevs/blog/core/rest/AppExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.wcdevs.blog.core.rest;
 
 import com.wcdevs.blog.core.common.exception.NotFoundException;
+import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import java.time.LocalDateTime;
 
 /**
  * Handles exceptions thrown by the business logic.
