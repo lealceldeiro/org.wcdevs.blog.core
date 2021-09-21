a1ef5e53f2c25ecf1a3e3cca672f5d94ff2a2b3a
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-21 14:44:53 +0300

    Update persistence module
    - Update repository query method

--- persistence/src/main/java/com/wcdevs/blog/core/persistence/user/UserRepository.java
+++ persistence/src/main/java/com/wcdevs/blog/core/persistence/user/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository to handle the DB interaction with the user table.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<UserDto> findByEmailOrUsername(String email, String username);
+  Optional<UserDto> findByEmailOrUsernameAllIgnoreCase(String email, String username);
 }
