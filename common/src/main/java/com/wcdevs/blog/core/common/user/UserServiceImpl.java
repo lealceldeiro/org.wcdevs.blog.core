88d41876f28087428fd5aff37a10e79cbb875a73
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-21 15:12:51 +0300

    Update rest module
    - Make service use @Transaction
    - Add spring.tx requires directive in module-info.java

--- common/src/main/java/com/wcdevs/blog/core/common/user/UserServiceImpl.java
+++ common/src/main/java/com/wcdevs/blog/core/common/user/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.wcdevs.blog.core.common.user;
 import com.wcdevs.blog.core.persistence.user.UserDto;
 import com.wcdevs.blog.core.persistence.user.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Default {@link UserService} implementation.
  */
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
 
--- common/src/main/java/module-info.java
+++ common/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 open module com.wcdevs.blog.core.common {
   requires transitive com.wcdevs.blog.core.persistance;
   requires spring.context;
+  requires spring.tx;
 
   exports com.wcdevs.blog.core.common.user;
   exports com.wcdevs.blog.core.common.exception;
