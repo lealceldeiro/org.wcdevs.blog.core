7300092dec68dbe6319247a00b0706b4fb2677fb
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-21 16:28:03 +0300

    Update common module
    - Require spring.aop

--- common/src/main/java/module-info.java
+++ common/src/main/java/module-info.java
@@ -2,6 +2,7 @@ open module com.wcdevs.blog.core.common {
   requires transitive com.wcdevs.blog.core.persistance;
   requires spring.context;
   requires spring.tx;
+  requires spring.aop;
 
   exports com.wcdevs.blog.core.common.user;
   exports com.wcdevs.blog.core.common.exception;
