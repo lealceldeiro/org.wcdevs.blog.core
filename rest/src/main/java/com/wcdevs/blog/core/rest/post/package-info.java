080d93f500eea73d1eab4fb30d15516528f9ac5b
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-09-22 18:54:27 +0300

    Update rest module
    - Create endpoint to get all posts
    - Move controller to own package

---
+++ rest/src/main/java/com/wcdevs/blog/core/rest/post/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes providing post-related webservices.
+ *
+ * @author Asiel Leal Celdeiro | lealceldeiro@gmail.com
+ * @version 0.1
+ */
+package com.wcdevs.blog.core.rest.post;
