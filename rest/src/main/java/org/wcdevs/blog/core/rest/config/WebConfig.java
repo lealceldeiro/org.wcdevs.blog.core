08ad751d319b6d13a98059291de5edb4893ae5f8
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-23 12:18:25 +0200

    Fix allowed origins config

--- rest/src/main/java/org/wcdevs/blog/core/rest/config/WebConfig.java
+++ rest/src/main/java/org/wcdevs/blog/core/rest/config/WebConfig.java
@@ -1,5 +1,8 @@
 package org.wcdevs.blog.core.rest.config;
 
+import static java.util.Collections.emptyList;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +13,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+  private static final List<String> RESOURCE_PATTERNS = List.of("/**");
   private final CoreProperties coreProperties;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    log.info("Adding CORS allowed origins {}", coreProperties.getAllowedOrigins());
-    coreProperties.getAllowedOrigins().forEach(registry::addMapping);
+    var origins = Optional.ofNullable(coreProperties.getAllowedOrigins()).orElse(emptyList());
+    log.info("Adding CORS allowed origins {} to {}", origins, RESOURCE_PATTERNS);
+    RESOURCE_PATTERNS
+        .forEach(path -> origins.forEach(registry.addMapping(path)::allowedOriginPatterns));
   }
 }
