ddba50a864b73e3092cc53498f23bb89886126f7
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-23 15:52:14 +0200

    Fix setting multiple allowed origins

--- rest/src/main/java/org/wcdevs/blog/core/rest/config/WebConfig.java
+++ rest/src/main/java/org/wcdevs/blog/core/rest/config/WebConfig.java
@@ -1,5 +1,6 @@
 package org.wcdevs.blog.core.rest.config;
 
+import java.util.Arrays;
 import static java.util.Collections.emptyList;
 import java.util.List;
 import java.util.Optional;
@@ -18,9 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    var origins = Optional.ofNullable(coreProperties.getAllowedOrigins()).orElse(emptyList());
-    log.info("Adding CORS allowed origins {} to {}", origins, RESOURCE_PATTERNS);
-    RESOURCE_PATTERNS
-        .forEach(path -> origins.forEach(registry.addMapping(path)::allowedOriginPatterns));
+    var origins = Optional.ofNullable(coreProperties.getAllowedOrigins())
+                          .orElse(emptyList())
+                          .toArray(new String[0]);
+    log.info("Adding CORS allowed origins {} to {}", Arrays.toString(origins), RESOURCE_PATTERNS);
+
+    RESOURCE_PATTERNS.forEach(path -> registry.addMapping(path).allowedOriginPatterns(origins));
   }
 }
