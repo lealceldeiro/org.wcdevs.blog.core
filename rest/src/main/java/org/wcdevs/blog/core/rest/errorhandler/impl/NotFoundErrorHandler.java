d8ad644ca0ec9954477cd2a140ae8819bea66e95
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-01 14:10:54 +0200

    refactor: Add NotFoundErrorHandler

---
+++ rest/src/main/java/org/wcdevs/blog/core/rest/errorhandler/impl/NotFoundErrorHandler.java
@@ -0,0 +1,26 @@
+package org.wcdevs.blog.core.rest.errorhandler.impl;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+import org.wcdevs.blog.core.common.exception.NotFoundException;
+import org.wcdevs.blog.core.rest.errorhandler.AbstractErrorHandler;
+import org.wcdevs.blog.core.rest.errorhandler.ErrorMessage;
+
+/**
+ * 404 error handler.
+ */
+public class NotFoundErrorHandler extends AbstractErrorHandler {
+  @Override
+  protected boolean canHandle(Throwable throwable) {
+    return throwable instanceof NotFoundException;
+  }
+
+  @Override
+  protected ResponseEntity<ErrorMessage> errorFrom(Throwable throwable, WebRequest request) {
+    var error = new ErrorMessage(throwable.getMessage(), request.getContextPath(),
+                                 LocalDateTime.now());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+}
