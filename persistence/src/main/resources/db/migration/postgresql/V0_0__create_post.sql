729e396143e4ab1fda208b89f1d3476e172cf346
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-12-05 21:23:52 +0200

    feat(22): Update create post table sql

--- persistence/src/main/resources/db/migration/postgresql/V0_0__create_post.sql
+++ persistence/src/main/resources/db/migration/postgresql/V0_0__create_post.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS post
   title        VARCHAR(250) NOT NULL UNIQUE,
   slug         VARCHAR(200) NOT NULL UNIQUE,
   body         TEXT         NOT NULL,
-  excerpt      VARCHAR(300) NOT NULL UNIQUE,
+  excerpt      VARCHAR(300) NOT NULL,
   published_on TIMESTAMP    NOT NULL,
   updated_on   TIMESTAMP    NOT NULL
 );
