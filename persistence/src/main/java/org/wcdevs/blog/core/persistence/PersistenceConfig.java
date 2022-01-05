b85b2d4a55e5d6c276a6758d9bde6afb449148be
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-01-05 18:25:09 +0200

    feat: Change bootstrapMode for JPA repositories
    - Set to DEFERRED

--- persistence/src/main/java/org/wcdevs/blog/core/persistence/PersistenceConfig.java
+++ persistence/src/main/java/org/wcdevs/blog/core/persistence/PersistenceConfig.java
@@ -3,12 +3,14 @@ package org.wcdevs.blog.core.persistence;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 
 /**
  * Config class to allow components framework scan.
  */
-@Configuration
-@EnableJpaRepositories(basePackages = "org.wcdevs.blog.core.persistence")
 @EntityScan
+@Configuration
+@EnableJpaRepositories(basePackages = "org.wcdevs.blog.core.persistence",
+                       bootstrapMode = BootstrapMode.DEFERRED)
 public class PersistenceConfig {
 }
