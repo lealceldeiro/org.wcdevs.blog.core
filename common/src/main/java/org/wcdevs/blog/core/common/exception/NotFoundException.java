5b2055dd25fdfb755920da1bfd033fe02df5935c
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-07-19 14:03:52 +0300

    refactor: Improve code after Java upgrade
    - Use newly added java.io.Serial annotation

--- common/src/main/java/org/wcdevs/blog/core/common/exception/NotFoundException.java
+++ common/src/main/java/org/wcdevs/blog/core/common/exception/NotFoundException.java
@@ -4,6 +4,7 @@ package org.wcdevs.blog.core.common.exception;
  * Exception thrown when an entity is not found.
  */
 public class NotFoundException extends RuntimeException {
+  @Serial
   private static final long serialVersionUID = 6496139832556491573L;
 
   public NotFoundException(String entityName) {
--- common/src/main/java/org/wcdevs/blog/core/common/post/PostNotFoundException.java
+++ common/src/main/java/org/wcdevs/blog/core/common/post/PostNotFoundException.java
@@ -6,6 +6,7 @@ import org.wcdevs.blog.core.common.exception.NotFoundException;
  * Exception thrown when a post is not found.
  */
 public class PostNotFoundException extends NotFoundException {
+  @Serial
   private static final long serialVersionUID = 5864134251188658316L;
 
   public PostNotFoundException() {
